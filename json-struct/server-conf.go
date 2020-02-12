0f5b54d7282438dc1bf2012bbf264edf7c3274a1
lightbrother <892217655@qq.com>
2020-02-12 16:58:26 +0800

    添加json描述

--- json-struct/server-conf.go
+++ json-struct/server-conf.go
@@ -3,31 +3,31 @@ package json_struct
 type ServerConf struct {
 	// Name
 	// 服务器名称
-	Name string
+	Name string `json:"name"`
 
 	// CmdStr
 	// 执行的完整命令
 	// 下标为0： 命令名称
 	// 大于0为命令参数
-	CmdStr []string
+	CmdStr []string `json:"cmdStr"`
 
 	// Port
 	// 启动服务器端口
-	Port int
+	Port int `json:"port"`
 
 	// RunPath
 	// 运行所在工作区间
-	RunPath string
+	RunPath string `json:"runPath"`
 
 	// IsMirror
 	// 是否是镜像服务器
-	IsMirror bool
+	IsMirror bool `json:"isMirror"`
 
 	// IsStartMonitor
 	// 是否启动资源监听器
-	IsStartMonitor bool
+	IsStartMonitor bool `json:"isStartMonitor"`
 
 	// Memory
 	// 使用内存大小，单位M
-	Memory int
+	Memory int `json:"memory"`
 }
