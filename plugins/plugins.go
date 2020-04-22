79bd417f1646df2cc93d212b951e2c0f4a4598f7
lightbrother <892217655@qq.com>
2020-04-22 19:29:25 +0800

    添加镜像插件

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -3,12 +3,13 @@ package plugins
 import (
 	plugin_interface "github.com/TISUnion/most-simple-mcd/interface/plugin"
 	"github.com/TISUnion/most-simple-mcd/plugins/broadcast"
+	mirror_server "github.com/TISUnion/most-simple-mcd/plugins/mirror-server"
 	reread_chicken "github.com/TISUnion/most-simple-mcd/plugins/reread-chicken"
 )
 
 var plugins = []plugin_interface.Plugin{
 	reread_chicken.RereadChickenPluginObj,
-	//mirror_server.GetMirrorServerPluginInstance(),
+	mirror_server.GetMirrorServerPluginInstance(),
 	BasicPluginObj,
 	broadcast.GetBroadcastPluginInstance(),
 }
