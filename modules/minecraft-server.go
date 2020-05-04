3dd7316f32a8d67e465449a7cfc653d3850b8dad
lightbrotherV <892217655@qq.com>
2020-05-04 14:51:50 +0800

    bug修复，兼容老版本

--- modules/minecraft-server.go
+++ modules/minecraft-server.go
@@ -16,7 +16,9 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -287,34 +289,26 @@ func (m *MinecraftServer) Restart() error {
 
 // 获取一条消息
 func (m *MinecraftServer) resiveOneMessage() ([]byte, error) {
-	const MAX_SIZE = 1024
-	buff := make([]byte, MAX_SIZE)
-	n, err := m.stdout.Read(buff)
-	buff = buff[:n]
-	if err != nil {
-		errMsg := fmt.Sprintf("服务器: %s，已关闭。因为%v", m.Name, err)
-		m.sureServerStop()
-		return []byte{}, errors.New(errMsg)
-	}
-	// 如果一次的数据为1024，就多次获取
-	if n == MAX_SIZE {
-		for {
-			subBuff := make([]byte, MAX_SIZE)
-			subN, subErr := m.stdout.Read(buff)
-			if subErr != nil {
-				errMsg := fmt.Sprintf("服务器: %s，已关闭。因为%v", m.Name, err)
-				m.sureServerStop()
-				return []byte{}, errors.New(errMsg)
-			}
-			subBuff = subBuff[:subN]
-			buff = append(buff, subBuff...)
-			if subN != MAX_SIZE {
-				break
-			}
+	result := make([]byte, 0)
+	for {
+		buff := make([]byte, constant.MAX_RESIVE_BUFF_SIZE)
+		n, err := m.stdout.Read(buff)
+		if err != nil {
+			errMsg := fmt.Sprintf("服务器: %s，已关闭。因为%v", m.Name, err)
+			m.sureServerStop()
+			return []byte{}, errors.New(errMsg)
+		}
+		if n <= 0 {
+			continue
+		}
+		result = append(result, buff[:n]...)
+		if buff[n-1] == '\n' {
+			break
 		}
 	}
-	buff, _ = utils.ParseCharacter(buff)
-	return buff, nil
+
+	result, _ = utils.ParseCharacter(result)
+	return result, nil
 }
 
 // 获取消息，并写入到管道中
@@ -400,7 +394,6 @@ func (m *MinecraftServer) sureServerStart(data []byte) {
 
 // 判断服务端是否已经关闭
 func (m *MinecraftServer) sureServerStop() {
-	m.State = constant.MC_STATE_START
 	m.stopTagChan <- struct{}{}
 }
 
@@ -425,7 +418,14 @@ func (m *MinecraftServer) Command(c string) error {
 func (m *MinecraftServer) _command(c string) error {
 	// 不加换行无法执行命令
 	c += "\n"
-	_, err := m.stdin.Write([]byte(c))
+	cmd := []byte(c)
+	sys := runtime.GOOS
+	// 在windows下小于1.12是gbk编码
+	if sys == constant.OS_WINDOWS && utils.CompareMcVersion(m.Version, constant.MC_LAST_UTF8_VERSION) == constant.COMPARE_LT {
+		c = strings.ReplaceAll(c, "\n", "\r\n")
+		cmd, _ = utils.UTF82GBK([]byte(c))
+	}
+	_, err := m.stdin.Write(cmd)
 	return err
 }
 
