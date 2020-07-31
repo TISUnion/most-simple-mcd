bbd74fc983f067372bb538ad5d4b40d62beb4369
lightbrother <892217655@qq.com>
2020-07-31 18:48:22 +0800

    添加事件正则

--- modules/server-adapter.go
+++ modules/server-adapter.go
@@ -5,7 +5,7 @@ import "github.com/TISUnion/most-simple-mcd/constant"
 type ServerAdapter struct {
 	side string
 }
-
+//================================启动
 // 获取版本
 func (sa *ServerAdapter) GetVersionRegularExpression() string {
 	switch sa.side {
@@ -46,13 +46,54 @@ func (sa *ServerAdapter) GetGameSaveRegularExpression() string {
 	}
 }
 
-// 获取玩家发言
+//=================================游戏事件
+// 获取玩家发言 [09:00:00] [Server thread/INFO]: <Steve> Hello
 func (sa *ServerAdapter) GetMessageRegularExpression() string {
 	switch sa.side {
 	case constant.VANILLA:
 		return constant.VANILLA_MESSAGE
 	default:
 		return ""
+	}
+}
+
+// 加入游戏 Steve joined the game
+func (sa *ServerAdapter) GetPlayerJoinRegularExpression() string {
+	switch sa.side {
+	case constant.VANILLA:
+		return constant.VANILLA_PLAYER_JOIN
+	default:
+		return ""
+	}
+}
+
+// 离开游戏 Steve left the game
+func (sa *ServerAdapter) GetPlayerLeftRegularExpression() string {
+	switch sa.side {
+	case constant.VANILLA:
+		return constant.VANILLA_PLAYER_LEFT
+	default:
+		return ""
+	}
+}
+
+// 获得成就 Steve has made the advancement [Stone Age]
+func (sa *ServerAdapter) GetPlayerAdvancementRegularExpression() string {
+	switch sa.side {
+	case constant.VANILLA:
+		return constant.VANILLA_PLAYER_ADVANCEMENT
+	default:
+		return ""
+	}
+}
+
+// 玩家死亡
+func (sa *ServerAdapter) GetPlayerDeathRegularExpression() (message []string) {
+	switch sa.side {
+	case constant.VANILLA:
+		return constant.DeathMessage
+	default:
+		return
 	}
 }
 
